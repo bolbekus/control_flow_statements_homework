64de597ac49700e19ab2244279cf8718ee42d272
bolbekus <bolbekus@gmail.com>
2023-01-18 16:32:47 +0500

    04

--- if04.py
+++ if04.py
@@ -8,4 +8,17 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    n = 0
+    if a > 0:
+        n += 1
+
+    if b > 0:
+        n += 1
+
+    if c > 0:
+        n += 1
+
+
+    return n
+
+print(main(-2,4,1))
