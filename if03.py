113ee1cd90801314878a8d5d78c9124aa30b54eb
bolbekus <bolbekus@gmail.com>
2023-01-18 15:07:13 +0500

    03

--- if03.py
+++ if03.py
@@ -1,6 +1,6 @@
 def main(a):
     """
-    If the number is positive, increase it to 1, else decrease it to 2. \
+    If the number is positive, increase it to 1, else decrease it to 2.
      If it is 0, assign 10.
     Args:
         a: integer
@@ -9,7 +9,12 @@ def main(a):
     """
     if a > 0:
         a = a + 1
+    if a < 0:
+        a = a - 2 
 
+    if a == 0:
+        a = 10
+       
     return a
 
 print(main(3))
